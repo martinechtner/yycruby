183b04a290924f6154716faa64f2dc9a1373f077
martinechtner <martin.echtner@gmail.com>
2016-10-05 17:48:29 -0600

    remove uneeded

--- spec/views/sponsors/show.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe 'sponsors/show', type: :view do
-  before(:each) do
-    @sponsor = assign(:sponsor, create(:sponsor))
-  end
-
-  subject { render }
-
-  include_examples 'a basic view'
-end
