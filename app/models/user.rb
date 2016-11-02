d406aa178c8a3990b5e31d7f85e6df7b865afa2f
martinechtner <martin.echtner@gmail.com>
2016-11-02 07:26:49 -0600

    remove devise recoverable and registerable
    no email setup currently

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 end
