e88df341bead7e0ae191bab74da6e524a64c2ad1
martinechtner <martin.echtner@gmail.com>
2018-12-20 08:32:53 -0700

    If no next meetup use downtown Calgary's lat and long.

--- app/controllers/concerns/generate_map_data.rb
+++ app/controllers/concerns/generate_map_data.rb
@@ -5,9 +5,16 @@ module GenerateMapData
   end
 
   def generate_json
-    Gmaps4rails.build_markers(@next_meetup) do |location, marker|
-      marker.lat location.venue['lat']
-      marker.lng location.venue['lon']
+    if @next_meetup.present?
+      Gmaps4rails.build_markers(@next_meetup) do |location, marker|
+        marker.lat location.venue['lat']
+        marker.lng location.venue['lon']
+      end
+    else
+      Gmaps4rails.build_markers(['placeholder']) do |_placeholder, marker|
+        marker.lat 51.0486151
+        marker.lng -114.0708459
+      end
     end
   end
 end
